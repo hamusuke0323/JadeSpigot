e985fd37645bffe0c3ac0abf0974d22494ecb30c
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2025-02-14 13:47:53 +0900

    fix: cooldown timer for trial spawner

--- src/main/java/com/hamusuke/jadespigot/addon/MobSpawnerCooldownProvider.java
+++ src/main/java/com/hamusuke/jadespigot/addon/MobSpawnerCooldownProvider.java
@@ -1,14 +1,19 @@
 package com.hamusuke.jadespigot.addon;
 
+import com.google.common.collect.Maps;
 import com.hamusuke.jadespigot.accessors.BlockAccessor;
 import com.hamusuke.jadespigot.providers.StreamServerDataProvider;
+import com.mojang.serialization.JavaOps;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.MinecraftKey;
 import net.minecraft.server.level.WorldServer;
 import net.minecraft.world.level.block.entity.TrialSpawnerBlockEntity;
+import net.minecraft.world.level.block.entity.trialspawner.TrialSpawnerData;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
 
 public enum MobSpawnerCooldownProvider implements StreamServerDataProvider<BlockAccessor, Integer> {
     INSTANCE;
@@ -17,11 +22,20 @@ public enum MobSpawnerCooldownProvider implements StreamServerDataProvider<Block
 
     @Override
     public @Nullable Integer streamData(BlockAccessor accessor) {
-        var spawner = (TrialSpawnerBlockEntity) accessor.getBlockEntity();
-        var spawnerData = spawner.c().f();
+        var spawnerBlock = (TrialSpawnerBlockEntity) accessor.getBlockEntity();
+        var spawner = spawnerBlock.c();
+        var spawnerData = spawner.f();
         var level = (WorldServer) accessor.getLevel();
-        if (spawner.c().a(level) && !spawnerData.a(level)) {
-            return (int) (spawner.c().d().a() - level.ad());
+
+        var result = TrialSpawnerData.b.encoder().encode(spawnerData, JavaOps.INSTANCE, Maps.newHashMap()).result();
+        if (result.isEmpty() || !(result.get() instanceof Map<?, ?> map)) {
+            return null;
+        }
+
+        var cooldownEndsAt = map.get("cooldown_ends_at");
+
+        if (cooldownEndsAt instanceof Long l && spawner.a(level) && !spawnerData.a(level)) {
+            return (int) (l - level.ad());
         }
 
         return null;
